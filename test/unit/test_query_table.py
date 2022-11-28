0850a3c71f160aa638bd98b515b50bda383a8c90
John <john.cormican@mpinat.mpg.de>
2022-11-28 16:29:59 +0100

    iBench-1.2 Dealing with splice reactant overlap when remapping to proteome.

--- test/unit/test_query_table.py
+++ test/unit/test_query_table.py
@@ -6,21 +6,21 @@ from ibench.constants import (
     CANONICAL_KEY,
     CISSPLICED_KEY,
 )
-from ibench.query_table import _remap_to_proteome
+from ibench.query_table import remap_to_proteome
 
 PROTEOME = ['MITTAGETLIREDIERSCHNELLRS', 'TEININGSEQADD', 'YKRKLMNPQRHAPPY']
 
 
 class TestQueryTable(unittest.TestCase):
-    def test__remap_to_proteome_can(self):
-        stratum = _remap_to_proteome('HAPPY', PROTEOME, 25)
+    def test_remap_to_proteome_can(self):
+        stratum = remap_to_proteome('HAPPY', PROTEOME, 25)
         self.assertEqual(stratum, CANONICAL_KEY)
 
-    def test__remap_to_proteome_cis(self):
-        stratum = _remap_to_proteome('TIRED', PROTEOME, 25)
+    def test_remap_to_proteome_cis(self):
+        stratum = remap_to_proteome('TIRED', PROTEOME, 25)
         self.assertEqual(stratum, CISSPLICED_KEY)
 
-    def test__remap_to_proteome_trans(self):
-        stratum = _remap_to_proteome('INRS', PROTEOME, 25)
+    def test_remap_to_proteome_trans(self):
+        stratum = remap_to_proteome('INRS', PROTEOME, 25)
         self.assertEqual(stratum, 'unknown')
 
